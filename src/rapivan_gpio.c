b81a2d78c2c8c8df4704ed7307f28d3a41ab898b
Ivan Spina <i.spina@live.com>
2022-08-01 01:11:56 +0200

    add fops functions

--- src/rapivan_gpio.c
+++ src/rapivan_gpio.c
@@ -3,38 +3,66 @@
 #include <linux/fs.h>
 #include <linux/init.h>
 #include <linux/device.h>
+#include <linux/atomic.h>
+#include <asm/uaccess.h>
 
 #define DRIVER_AUTHOR "klesk"
-
 #define DRV_NAME "RASPILED"
+
+#define GPIO_17 0x7E200014
+#define GPIO_17_SIZE 0x0000004
+#define LED_ON 1
+#define LED_OFF 0
 
 static dev_t led_dev_number;
 static struct cdev* driver_object;
 static struct class *led_class;
 static struct device* led_dev;
 
+static atomic_t access_count = ATOMIC_INIT(-1);
 
-// static int open_led(struct inode* device_file, struct file* instance) {
-//     printk("open_led(): drv open");
-//     return 0;
-// }
-// static int read_led(struct file* instance, char __user* userbuffer, size_t count, loff_t* offset) {
-//     return 0;
-// }
-// static int write_led(struct inode* device_file, struct file* instance) {
-//     return 0;
-// }
-// static int close_led(struct inode* device_file, struct file* instance) {
-//         printk("close_led(): drv close");
-//     return 0;
-// }
+static int open_gpio(struct inode* device_file, struct file* instance) {
+    printk("open_led(): drv open");
+
+    if(instance->f_flags&O_RDWR || instance->f_flags&O_WRONLY){
+        if(atomic_inc_and_test(&access_count))
+            return 0;
+    
+        atomic_dec(&access_count);
+        return -EBUSY;
+    }
+    return 0;
+}
+static ssize_t read_gpio(struct file* instance, char __user* userbuffer, size_t count, loff_t* offset) {
+    printk("read_led(): drv open");
+//TODO Read GPIO PIN STATUS
+    // unsigned long to_copy, not_copied;
+    // to_copy=min(count)
+    return 0;
+}
+static int write_gpio(struct inode* device_file, struct file* instance) {
+    printk("write_led(): drv open");
+    //TODO Write to GPIOPIN
+
+    return 0;
+}
+static int close_gpio(struct inode* device_file, struct file* instance) {
+    printk("close_led(): drv close");
+    
+    if(instance->f_flags&O_RDWR || instance->f_flags&O_WRONLY){
+        return 0;
+    }
+    atomic_dec(&access_count);
+
+    return 0;
+}
 
 static struct file_operations file_ops = {
     .owner = THIS_MODULE,
-    // .open = open_led,
-    // .read = read_led,
-    // .write= write_led,
-    // .release = close_led,
+    .open = open_gpio,
+    .read = read_gpio,
+    .write= write_gpio,
+    .release = close_gpio,
 };
 
 static int __init init_led(void){
@@ -57,7 +85,7 @@ static int __init init_led(void){
         goto free_cdev;
     //write entry sysfs for /dev/ entry
     //udev deamon will automatically load file into /dev/
-    led_class = class_create(THIS_MODULE,"Raspijingle");
+    led_class = class_create(THIS_MODULE,DRV_NAME);
     if(IS_ERR( led_class))
         goto free_cdev;
     led_dev = device_create(led_class,NULL,led_dev_number,NULL,"%s","raspijingle");
